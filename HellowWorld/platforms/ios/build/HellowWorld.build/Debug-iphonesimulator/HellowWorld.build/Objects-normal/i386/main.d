747cc9872ed3c47e427d8913397f141b987b7a4f
tuyenv <bitworkvn@gmail.com>
2017-07-03 08:06:10 +0700

    groceries app

---
+++ HellowWorld/platforms/ios/build/HellowWorld.build/Debug-iphonesimulator/HellowWorld.build/Objects-normal/i386/main.d
@@ -0,0 +1,9 @@
+dependencies: \
+  /Volumes/www/github/nativescript/HellowWorld/platforms/ios/internal/main.m \
+  /Volumes/www/github/nativescript/HellowWorld/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Volumes/www/github/nativescript/HellowWorld/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Volumes/www/github/nativescript/HellowWorld/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Volumes/www/github/nativescript/HellowWorld/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Volumes/www/github/nativescript/HellowWorld/platforms/ios/internal/TNSExceptionHandler.h \
+  /Volumes/www/github/nativescript/HellowWorld/platforms/ios/internal/TNSDebugging.h \
+  /Volumes/www/github/nativescript/HellowWorld/platforms/ios/internal/TKLiveSync/include/TKLiveSync.h
